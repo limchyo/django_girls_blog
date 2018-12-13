cf74e89ee3ec203b4912d6d4c8ca1c94694cbee9
Chaeoh <limchyo>
2018-12-13 23:49:11 +0900

    added form

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+def post_list(request):
+    ctx = {}
+    post = Post.objects.all()
+    post = post.filter(published_date__lte=timezone.now())
+    post = post.order_by('published_date')
+
+    ctx.update({ "post" : post })
+    return render(request, "post_list.html", ctx)
+
+def post_detail(request, pk):
+    ctx = {}
+    post = get_object_or_404(Post, id=pk) # 객체가 없는 경우 404 처리
+    # post = Post.objects.get(id=pk)
+    ctx.update({ "post" : post })
+    return render(request, "post_detail.html", ctx)
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", post.id) #post_detail URL에서 id값 먹인 곳
+
+    else:
+        form = PostForm() # 기본 폼은 비어있으므로 저장 실패시 빈 폼
+
+    ctx = { "form" : form }
+    return render(request, "post_edit.html", ctx)
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, id=pk) # 객체가 없는 경우 404 처리
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", post.id)
+    else:
+        form = PostForm(instance=post)
+        
+    ctx = { "form" : form }
+    return render(request, "post_edit.html", ctx)
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -119,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
